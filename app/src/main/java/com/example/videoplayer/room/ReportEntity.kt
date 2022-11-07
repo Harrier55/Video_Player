ab38fb399ba53116a4d25943b82f865ff043ce4d
Harrier55 <harrier55@yandex.ru>
2022-11-07 22:18:14 +0500

    в базе данных добавлены аннотации

--- app/src/main/java/com/example/videoplayer/room/ReportEntity.kt
+++ app/src/main/java/com/example/videoplayer/room/ReportEntity.kt
@@ -1,14 +1,21 @@
 package com.example.videoplayer.room
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
+
+private const val NOTE_COLUMN_ID_VIDEO = "id_video"
+private const val NOTE_COLUMN_NAME_VIDEO = "video_name"
+private const val NOTE_COLUMN_START_TIME = "start_time"
 
 @Entity
 data class ReportEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
+    @ColumnInfo(name = NOTE_COLUMN_ID_VIDEO)
     val id_video:Int,
+    @ColumnInfo(name = NOTE_COLUMN_NAME_VIDEO)
     val video_name:String,
+    @ColumnInfo(name = NOTE_COLUMN_START_TIME)
     val start_time: String
 )
